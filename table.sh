e4e8b7807ce6813b1c86433ec901e2d7af48a936
Ubuntu <ubuntu@ip-172-31-20-81.ec2.internal>
2024-09-07 04:52:44 +0000

    Homework11

---
+++ table.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+n1=5
+for n in {1..10}; do
+	echo "$n1 multiply by $n  =" $((n*n1))
+done
+
+m1=6
+for m in {1..10}; do
+	echo "$m1 multiply by $m  =" $((m*m1))
+done
+
+l1=7
+for l in {1..10}; do
+	echo "$l1 multiply by $l  =" $((l*l1))
+done
+
