12c742422981556d952f04d3695e3efd0f96e671
BohdanMatiuk <mbs.dvp@gmail.com>
2024-09-09 17:30:34 +0000

    steak done

---
+++ Homework13/steak.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Enter meat temperature  " temp
+
+if [ $temp -ge 120 ] && [ $temp -le 130 ]; then
+	echo "Rare"
+elif [ $temp -ge 131 ] && [ $temp -le 140 ]; then
+	echo "Medimum Rare"
+elif [ $temp -ge 141 ] && [ $temp -le 150 ]; then
+	echo "Medium"
+elif [ $temp -ge 151 ] && [ $temp -le 160 ]; then
+	echo "Medium well"
+elif [ $temp -ge 161 ] && [ $temp -le 170 ]; then
+	echo "Well Done"
+elif [ $temp -lt 120 ] || [ $temp -gt 170 ]; then
+	echo "stay away from a kitchen"
+
+fi
+
