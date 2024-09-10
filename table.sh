8ce81205a3436676bc7c2dd09d228a0f999e17b0
BohdanMatiuk <mbs.dvp@gmail.com>
2024-09-10 00:55:23 +0000

    new table

--- table.sh
+++ table.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-n1=5
+
 for n in {1..10}; do
-	echo "$n1 multiply by $n  =" $((n*n1))
+	echo "5 multiply by $n  =" $((5*n))
 done
 
-m1=6
-for m in {1..10}; do
-	echo "$m1 multiply by $m  =" $((m*m1))
+for n in {1..10}; do
+	echo "6 multiply by $n  =" $((6*n))
 done
 
-l1=7
-for l in {1..10}; do
-	echo "$l1 multiply by $l  =" $((l*l1))
+for n in {1..10}; do
+	echo "7  multiply by $n  =" $((7*n))
 done
 
